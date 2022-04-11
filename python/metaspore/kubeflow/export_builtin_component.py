8d04dd5b74242f72099097dc79d4bc7ee09bd695
HarrisonKim1969 <kimheavy@yahoo.com>
2022-04-11 18:26:06 +0800

    offline_experiment_pipeline

--- python/metaspore/kubeflow/export_builtin_component.py
+++ python/metaspore/kubeflow/export_builtin_component.py
@@ -1,16 +1,20 @@
 import os
 import kfp
 import sys
-# using a loop to check the current path
+# using a loop to check the /metaspore/algos/components
 for module in os.listdir(os.getcwd() + "/metaspore/algos/components"):
+    # just select the .py file, eg: WideDeep.py
     if module == '__init__.py' or module[-3:] != '.py':
         continue
+    # import that module, eg: WideDeep
     sys.path.append('./metaspore/algos/components')
     target_module = __import__(module[:-3], locals(), globals())
+    # check all the func in that module
     for func_name in dir(target_module):
-        print(func_name)
         func = getattr(target_module, func_name)
+        # check if it is decorated
         if hasattr(func,'is_decorated'):
+            # use the .component_name attributes
             kfp.components.func_to_container_op(
                 func = func,
                 base_image = 'hub.kce.ksyun.com/dmetasoul/spark:dmetasoul-v1.2.2-test',
