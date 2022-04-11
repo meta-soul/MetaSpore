1337c0ec7f2c0e7144481e29b3e8e874d7f5f6ea
HarrisonKim1969 <kimheavy@yahoo.com>
2022-04-11 12:02:33 +0800

    offline_experiment_pipeline

--- python/metaspore/__init__.py
+++ python/metaspore/__init__.py
@@ -101,6 +101,9 @@ from . import output
 from . import spark
 from . import patching_pickle
 from . import demo
+from . import kubeflow
+from . import pipeline
+from . import algos
 
 patching_pickle._patch_lookup_module_and_qualname()
 patching_pickle._patch_getsourcelines()
