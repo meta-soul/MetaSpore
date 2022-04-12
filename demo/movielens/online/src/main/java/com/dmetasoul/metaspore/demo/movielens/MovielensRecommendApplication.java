c9da10b6e64ab3b31f97b755e5f4a9b488096eee
LiuSY <2502794298@qq.com>
2022-04-12 15:26:40 +0800

    update metaspore

--- demo/movielens/online/src/main/java/com/dmetasoul/metaspore/demo/movielens/MovielensRecommendApplication.java
+++ demo/movielens/online/src/main/java/com/dmetasoul/metaspore/demo/movielens/MovielensRecommendApplication.java
@@ -19,7 +19,6 @@ package com.dmetasoul.metaspore.demo.movielens;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
 @SpringBootApplication(scanBasePackages = {"com.dmetasoul.metaspore"}, exclude = {DataSourceAutoConfiguration.class})
 public class MovielensRecommendApplication {
 
