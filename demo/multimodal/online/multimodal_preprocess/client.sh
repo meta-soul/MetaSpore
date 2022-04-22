ec576a5fc3799734c4bba153a487f49cadd8cfab
wikty <wikty@users.noreply.github.com>
2022-04-22 14:44:53 +0800

    update multimodal preprocess

--- demo/multimodal/online/multimodal_preprocess/client.sh
+++ demo/multimodal/online/multimodal_preprocess/client.sh
@@ -1,8 +1,12 @@
-model_key=bert
+model_key=sbert-chinese-qmc-domain-v1
+
+MY_S3_PATH='your S3 bucket'
 
 # Push tokenizer model to service
-model_tar_url=http://172.31.0.197:8081/bert_tokenizer.tar.gz
-python client.py push ${model_key} ${model_tar_url}
+#model_tar_url=http://172.31.0.197:8081/bert_tokenizer.tar.gz
+#python client.py push ${model_key} ${model_tar_url}
+aws s3 cp ${MY_S3_PATH}/demo/nlp-algos-transformer/models/sbert-chinese-qmc-domain-v1/sbert-chinese-qmc-domain-v1.tar.gz ./
+python client.py push ${model_key} ./sbert-chinese-qmc-domain-v1.tar.gz
 
 # Call tokenizer preprocessor
 python client.py tokenize ${model_key} 北京天安门
--- demo/multimodal/online/multimodal_preprocess/server.sh
+++ demo/multimodal/online/multimodal_preprocess/server.sh
@@ -1,1 +1,1 @@
-python server.py 50051 ./tmp 10
+python server.py 60051 ./tmp 10
