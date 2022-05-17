8f7fd224d6be274ef63e8891f2ac702cfa110610
Hades-888 <jiang.2826379@163.com>
2022-05-17 19:03:50 +0800

    fix cases from code review

--- python/scripts/consul/create_consul_watch.sh
+++ python/scripts/consul/create_consul_watch.sh
@@ -1,3 +1,3 @@
-/bin/sh
+#!/bin/bash
 set -x
-consul watch -http-addr=http://${CONSUL_IP}:8500 -type keyprefix -prefix dev/ curl -H 'Content-Type:application/json' -X POST --data-binary @- http://${POD_IP}:8080/notify
+consul watch -http-addr=http://${CONSUL_IP}:8500 -type keyprefix -prefix ${CONSUL_KEY_PREFIX} curl -H 'Content-Type:application/json' -X POST --data-binary @- http://${POD_IP}:8080/notify
