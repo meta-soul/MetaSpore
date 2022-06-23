39629fb70a1386e8eef242541b1f1184971753ac
andafterall <andafterall@msn.com>
2022-06-23 10:30:33 +0000

    update pipeline nodes

--- solutions/recommend/offline/pipelines/nodes/two_towers_estimator.py
+++ solutions/recommend/offline/pipelines/nodes/two_towers_estimator.py
@@ -1,13 +1,17 @@
 from .node import PipelineNode
 import metaspore as ms
+from ..utils import get_class
 
 class TwoTowersEstimatorNode(PipelineNode):
     def __call__(self, **payload) -> dict:
         conf = payload['conf']
-        
-        from python.dssm_net import UserModule, ItemModule, SimilarityModule
-        from python.training import TwoTowerBatchNegativeSamplingAgent, TwoTowerBatchNegativeSamplingModule
-        
+        UserModule = get_class(conf['user_module_class'])
+        ItemModule = get_class(conf['item_module_class'])
+        SimilarityModule = get_class(conf['similarity_module_class'])
+        TwoTowerRetrievalModule = get_class(conf['two_tower_retrieval_module_class'])
+        TwoTowerAgentModule = get_class(conf['two_tower_agent_class'])
+        TwoTowerEstimatorModule = get_class(conf['two_tower_estimator_class'])
+
         ## init user module, item module, similarity module
         user_module = UserModule(conf['user_column_name'], \
                                  conf['user_combine_schema'], \
@@ -26,18 +30,11 @@ class TwoTowersEstimatorNode(PipelineNode):
                                  l2 = conf['ftrl_l2_regularization'], \
                                  dense_structure = conf['dense_structure'])
         similarity_module = SimilarityModule(conf['tau'])
-        module = TwoTowerBatchNegativeSamplingModule(user_module, item_module, similarity_module)
+        module = TwoTowerRetrievalModule(user_module, item_module, similarity_module)
 
-        import importlib
-        module_lib = importlib.import_module(conf['two_tower_module'])
-        ## init estimator class
-        estimator_class_ = getattr(module_lib, conf['two_tower_estimator_class'])
-        estimator = estimator_class_(module = module,
+        estimator = TwoTowerEstimatorModule(module = module,
                                      item_dataset = payload['item_dataset'],
-                                     item_ids_column_indices = [2],
-                                     retrieval_item_count = 20,
-                                     metric_update_interval = 500,
-                                     agent_class = TwoTowerBatchNegativeSamplingAgent,
+                                     agent_class = TwoTowerAgentModule,
                                      **conf)
         ## dnn learning rate
         estimator.updater = ms.AdamTensorUpdater(conf['adam_learning_rate'])
