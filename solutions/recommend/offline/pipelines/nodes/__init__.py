4583d376bafaec2b9ab67b4c93ae13bcc529bb11
Sun Kai <intelligence.gear@gmail.com>
2022-06-23 03:18:22 +0000

    update pipelines.

---
+++ solutions/recommend/offline/pipelines/nodes/__init__.py
@@ -0,0 +1,5 @@
+from .node import PipelineNode
+from .data_loader import DataLoaderNode
+from .init_spark import InitSparkNode
+from .retrieval_evaluator import RetrievalEvaluatorNode
+from .two_towers_estimator import TwoTowersEstimatorNode
