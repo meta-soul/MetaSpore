9876cad212db8e65506705cddf32a2bc9512be53
intelligencegear <intelligence.gear@gmail.com>
2022-06-27 18:08:25 +0800

    update class utils.

--- solutions/recommend/offline/pipelines/utils/class_utils.py
+++ solutions/recommend/offline/pipelines/utils/class_utils.py
@@ -4,3 +4,8 @@ def get_class(class_dict):
     clazz = getattr(importlib.import_module(class_dict['module_name']), class_dict['class_name'])
     print('Debug - clazz: ', clazz)
     return clazz
+
+def get_class(module_name, class_name):
+    clazz = getattr(importlib.import_module(module_name), class_name)
+    print('Debug - clazz: ', clazz)
+    return clazz
