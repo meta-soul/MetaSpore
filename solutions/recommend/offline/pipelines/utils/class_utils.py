1da169eeb6619365671032cd6a66c0b647504b03
andafterall <andafterall@msn.com>
2022-06-28 03:58:56 +0000

    use new pipeline engine

--- solutions/recommend/offline/pipelines/utils/class_utils.py
+++ solutions/recommend/offline/pipelines/utils/class_utils.py
@@ -16,11 +16,6 @@
 
 import importlib
 
-def get_class(class_dict):
-    clazz = getattr(importlib.import_module(class_dict['module_name']), class_dict['class_name'])
-    print('Debug - clazz: ', clazz)
-    return clazz
-
 def get_class(module_name, class_name):
     clazz = getattr(importlib.import_module(module_name), class_name)
     print('Debug - clazz: ', clazz)
