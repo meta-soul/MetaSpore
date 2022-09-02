cfab54b343d94740d0ca2eb400b246717fee3876
codingfun2022 <codingfun2022@163.com>
2022-09-02 17:34:57 +0800

    Update to use builtin metaspore.TwoTowerRetrievalEstimator.

--- python/algos/twotower/__init__.py
+++ python/algos/twotower/__init__.py
@@ -13,5 +13,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-from .two_tower_retrieval import TwoTowerRetrievalModule, TwoTowerRetrievalModel, TwoTowerRetrievalEstimator
