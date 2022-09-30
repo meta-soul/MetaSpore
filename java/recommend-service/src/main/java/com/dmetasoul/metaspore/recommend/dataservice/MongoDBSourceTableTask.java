aefe23178bf3cdcd34f95ec1877377af96eddb50
qinyy907 <yuanyq@dmetasoul.com>
2022-09-30 17:38:11 +0800

    fix bug about query duolicate

--- java/recommend-service/src/main/java/com/dmetasoul/metaspore/recommend/dataservice/MongoDBSourceTableTask.java
+++ java/recommend-service/src/main/java/com/dmetasoul/metaspore/recommend/dataservice/MongoDBSourceTableTask.java
@@ -24,6 +24,7 @@ import com.dmetasoul.metaspore.recommend.datasource.MongoDBSource;
 import com.dmetasoul.metaspore.recommend.enums.ConditionTypeEnum;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import lombok.extern.slf4j.Slf4j;
@@ -116,7 +117,8 @@ public class MongoDBSourceTableTask extends SourceTableTask {
     private void fillDocument(String col, Object value) {
         if (value instanceof Collection) {
             BasicDBList values = new BasicDBList();
-            values.addAll((Collection) value);
+            HashSet valueSet = Sets.newHashSet((Collection) value);
+            values.addAll(valueSet);
             BasicDBObject in = new BasicDBObject("$in", values);
             queryObject.put(col, in);
         } else {
