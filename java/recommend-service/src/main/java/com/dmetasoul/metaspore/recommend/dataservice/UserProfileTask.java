ca85f5662c79fae6dae878f54e24954c8cb9dceb
yuanyq <yuanyq@dmetasoul.com>
2022-10-04 21:44:07 +0800

    fix bug

--- java/recommend-service/src/main/java/com/dmetasoul/metaspore/recommend/dataservice/UserProfileTask.java
+++ java/recommend-service/src/main/java/com/dmetasoul/metaspore/recommend/dataservice/UserProfileTask.java
@@ -19,6 +19,7 @@ import com.dmetasoul.metaspore.recommend.annotation.ServiceAnnotation;
 import com.dmetasoul.metaspore.recommend.common.CommonUtils;
 import com.dmetasoul.metaspore.recommend.configure.FieldInfo;
 import com.dmetasoul.metaspore.recommend.enums.DataTypeEnum;
+import com.dmetasoul.metaspore.recommend.data.TableData;
 import com.google.common.collect.Lists;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -65,21 +66,26 @@ public class UserProfileTask extends AlgoTransformTask {
             Map<String, Object> options = config.getOptions();
             List<FieldInfo> fields = config.getInputFields();
             Assert.isTrue(CollectionUtils.isNotEmpty(fields), "input data is not null");
-            int num = 0;
-            List<List<List<Object>>> data = Lists.newArrayList();
+            TableData recallData = new TableData();
+            List<String> names = config.getNames();
+            List<Object> types = config.getTypes();
+            Assert.isTrue(names.size() > 2, "output has 3 fields");
+	    int num = 0;
             for (int i = 0; i < fieldTableData.getData().size(); ++i) {
-                List<Object> list = (List<Object>) fieldTableData.getValue(i, fields.get(0));
+                List<Object> list = (List<Object>) fieldTableData.getValue(i, fields.get(1));
                 if (list == null) {
                     continue;
                 }
-                List<Object> weights = Lists.newArrayList();
                 for (int k = 0; k < list.size(); ++k) {
-                    weights.add(1 / (1 + Math.pow((list.size() - k - 1), alpha)));
+                    double weight = 1 / (1 + Math.pow((list.size() - k - 1), alpha));
+		    recallData.setValue(num, names.get(0), types.get(0), fieldTableData.getValue(i, fields.get(0)));
+                    recallData.setValue(num, config.getNames().get(1), config.getTypes().get(1), list.get(k));
+                    recallData.setValue(num, config.getNames().get(2), config.getTypes().get(2), weight);
+		    num += 1;
                 }
-                data.add(List.of(list, weights));
             }
-            List<FieldInfo> fieldInfos = config.getNames().stream().map(FieldInfo::new).collect(Collectors.toList());
-            fieldTableData.flatListValue(data, fieldInfos, config.getTypes());
+            fieldTableData.reset(recallData);
+            // fieldTableData.flatListValue(data, fieldInfos, config.getTypes());
             return true;
         });
     }
