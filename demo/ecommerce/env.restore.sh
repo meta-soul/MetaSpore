5a2157bd86a7fa9157f0c2b21425b25a6f46ccc6
codingfun2022 <codingfun2022@163.com>
2022-10-10 16:17:26 +0800

    Use python3 command to create venv.

--- demo/ecommerce/env.restore.sh
+++ demo/ecommerce/env.restore.sh
@@ -2,7 +2,7 @@
 
 set -e
 rm -rf $(realpath $(dirname ${BASH_SOURCE[0]}))/python-env
-python -m venv $(realpath $(dirname ${BASH_SOURCE[0]}))/python-env
+python3 -m venv $(realpath $(dirname ${BASH_SOURCE[0]}))/python-env
 source $(realpath $(dirname ${BASH_SOURCE[0]}))/python-env/bin/activate
 python -m pip install ../../python/metasporecli/metasporecli-0.1.0-py3-none-any.whl  \
                       ../../python/metasporecli/metasporeflow-0.1.0-py3-none-any.whl
