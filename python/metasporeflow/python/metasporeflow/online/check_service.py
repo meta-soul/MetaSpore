1df9b99d913d3bea5aee27ff0faa0ecb96a457df
codingfun2022 <codingfun2022@163.com>
2022-10-10 11:41:22 +0800

    Catch exception in notifyRecommendService.

--- python/metasporeflow/python/metasporeflow/online/check_service.py
+++ python/metasporeflow/python/metasporeflow/online/check_service.py
@@ -1,21 +1,31 @@
 import requests
 import time
+import traceback
 
 def notifyRecommendService(host, port):
     print("notify recommend service: %s:%s" % (host, port))
-    num = 14
+    num = 300
+    last_exception = None
     while num > 0:
-        resp = requests.post('http://%s:%s/actuator/refresh' % (host, port))
-        if resp.status_code == 200:
+        try:
+            resp = requests.post('http://%s:%s/actuator/refresh' % (host, port))
+        except Exception as ex:
+            resp = None
+            last_exception = ex
+        if resp is not None and resp.status_code == 200:
             try:
                 data = resp.json()
+            except Exception as ex:
+                data = None
+                last_exception = ex
+            if data is not None:
                 print(data)
-            except:
-                pass
-            break
+                return
         print("retry refresh recommend service! %s:%s" % (host, port))
         time.sleep(1)
         num -= 1
+    if last_exception is not None:
+        traceback.print_exception(last_exception)
 
 if __name__ == "__main__":
     print("test")
