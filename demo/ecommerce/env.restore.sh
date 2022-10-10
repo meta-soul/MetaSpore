683b8e2ae3d5e8fbcb74f95fb8f1e7f6826e332a
codingfun2022 <codingfun2022@163.com>
2022-10-10 17:20:51 +0800

    Use official metasporecli.

--- demo/ecommerce/env.restore.sh
+++ demo/ecommerce/env.restore.sh
@@ -4,6 +4,5 @@ set -e
 rm -rf $(realpath $(dirname ${BASH_SOURCE[0]}))/python-env
 python3 -m venv $(realpath $(dirname ${BASH_SOURCE[0]}))/python-env
 source $(realpath $(dirname ${BASH_SOURCE[0]}))/python-env/bin/activate
-python -m pip install ../../python/metasporecli/metasporecli-0.1.0-py3-none-any.whl  \
-                      ../../python/metasporecli/metasporeflow-0.1.0-py3-none-any.whl
+python -m pip install metasporecli==0.1.0
 python -m pip freeze > $(realpath $(dirname ${BASH_SOURCE[0]}))/python-env/requirements.txt
