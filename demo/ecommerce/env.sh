3cf500c9df98c46666567e899d1535d9eae93b98
Hades-888 <jiang.2836379@163.com>
2022-10-10 12:51:30 +0800

    update online image

--- demo/ecommerce/env.sh
+++ demo/ecommerce/env.sh
@@ -8,5 +8,5 @@ source $(realpath $(dirname ${BASH_SOURCE[0]}))/python-env/bin/activate
 export MONGODB_IMAGE="mongo:5.0"
 export MYSQL_IMAGE="mysql:5.7"
 export OFFLINE_IMAGE="swr.cn-southwest-2.myhuaweicloud.com/dmetasoul-repo/metaspore-training-release:v1.1.0"
-export ONLINE_IMAGE="swr.cn-southwest-2.myhuaweicloud.com/dmetasoul-repo/recommend-service-11:1.0.14"
+export ONLINE_IMAGE="swr.cn-southwest-2.myhuaweicloud.com/dmetasoul-repo/recommend-service:1.0.0"
 export FRONT_IMAGE="swr.cn-southwest-2.myhuaweicloud.com/dmetasoul-repo/ecommerce-vue-app:v1.0.0"
--- demo/ecommerce/stop.sh
+++ demo/ecommerce/stop.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+source ./env.sh
+
 docker compose -f init_container/create-database.yml down
 
-source ./env.sh
 cd ecommerce_demo || exit
 metaspore flow down
