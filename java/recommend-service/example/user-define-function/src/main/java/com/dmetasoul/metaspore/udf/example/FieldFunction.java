1f76fc9fdce8e4a3d4235abc7a53406aa6b5df1f
qinyuanyuan <qinyy@ushareit.com>
2022-10-11 10:39:26 +0800

    update udf example code

--- java/recommend-service/example/user-define-function/src/main/java/com/dmetasoul/metaspore/udf/example/FieldFunction.java
+++ java/recommend-service/example/user-define-function/src/main/java/com/dmetasoul/metaspore/udf/example/FieldFunction.java
@@ -13,13 +13,8 @@ import java.util.List;
 @Slf4j
 public class FieldFunction implements Function {
     @Override
-    public boolean process(@NotEmpty List<FieldData> list, @NotEmpty List<FieldData> list1, @NonNull FieldAction fieldAction) {
-        Validate.isTrue(list.size() == list1.size(), "in and out must be same size");
+    public boolean process(@NonNull TableData fieldTableData, @NonNull FieldAction config, @NonNull ExecutorService taskPool) {
         log.info("******************this is example field process function!*******************");
-        for (int i = 0; i < list.size(); ++i) {
-            FieldData item = list.get(i);
-            list1.get(0).setIndexValue(item.getIndexValue());
-        }
         return true;
     }
 }
