85462e95916c5a6c7068a700679e4f1f17d05bab
codingfun2022 <codingfun2022@163.com>
2022-11-21 03:31:44 +0000

    Set AWS_DEFAULT_REGION manually to make crontab work.

--- python/metasporeflow/python/metasporeflow/runners/crontab_sage_maker_runner.py
+++ python/metasporeflow/python/metasporeflow/runners/crontab_sage_maker_runner.py
@@ -171,6 +171,17 @@ class CrontabSageMakerRunner(object):
         print(job_name)
 
     def run(self):
+        import os
+        #import io
+        #import sys
+        #import datetime
+        #tag = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+        #stdout_path = os.path.expanduser('~/stdout%s.txt' % tag)
+        #stderr_path = os.path.expanduser('~/stderr%s.txt' % tag)
+        #sys.stdout = io.open(stdout_path, 'a')
+        #sys.stderr = io.open(stderr_path, 'a')
+        os.environ['AWS_DEFAULT_REGION'] = 'cn-northwest-1'
+
         self._parse_args()
         self._load_sage_maker_config()
         self._create_training_job()
