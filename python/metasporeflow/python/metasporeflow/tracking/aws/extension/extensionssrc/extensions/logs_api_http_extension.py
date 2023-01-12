bf5e79dd5a0e92b9d3acda66975cb8df0034f5ef
jnj <jiang.2826379@163.com>
2023-01-12 11:34:50 +0800

    update tracking count & time

--- python/metasporeflow/python/metasporeflow/tracking/aws/extension/extensionssrc/extensions/logs_api_http_extension.py
+++ python/metasporeflow/python/metasporeflow/tracking/aws/extension/extensionssrc/extensions/logs_api_http_extension.py
@@ -5,12 +5,13 @@ import os
 import sys
 from pathlib import Path
 from datetime import datetime
+
 # Add lib folder to path to import boto3 library.
 # Normally with Lambda Layers, python libraries are put into the /python folder which is in the path.
 # As this extension is bringing its own Python runtime, and running a separate process, the path is not available.
 # Hence, having the files in a different folder and adding it to the path, makes it available. 
 lib_folder = Path(__file__).parent / "lib"
-sys.path.insert(0,str(lib_folder))
+sys.path.insert(0, str(lib_folder))
 import boto3
 
 from logs_api_http_extension.http_listener import http_server_init, RECEIVER_PORT
@@ -45,9 +46,10 @@ Note:
 
 """
 
+
 class LogsAPIHTTPExtension():
     def __init__(self, agent_name, registration_body, subscription_body):
- #       print(f"extension.logs_api_http_extension: Initializing LogsAPIExternalExtension {agent_name}")
+        #       print(f"extension.logs_api_http_extension: Initializing LogsAPIExternalExtension {agent_name}")
         self.agent_name = agent_name
         self.queue = Queue()
         self.logs_api_client = LogsAPIClient()
@@ -57,7 +59,7 @@ class LogsAPIHTTPExtension():
         self.agent_id = self.extensions_api_client.register(self.agent_name, registration_body)
 
         # Start listening before Logs API registration
-#        print(f"extension.logs_api_http_extension: Starting HTTP Server {agent_name}")
+        #        print(f"extension.logs_api_http_extension: Starting HTTP Server {agent_name}")
         http_server_init(self.queue)
         self.logs_api_client.subscribe(self.agent_id, subscription_body)
 
@@ -73,6 +75,30 @@ class LogsAPIHTTPExtension():
         collection_table = self._mongo_client[os.environ['METASPOREFLOW_TRACKING_DB_TABLE']]
         collection_table.insert_one(eval(record))
 
+    def _update_tracking_user_bhv_to_mongodb(self, user_id, item_id, time):
+        from dateutil import parser
+        item_query_id = "items_bhv." + item_id
+        item_query_time = item_query_id + ".time"
+        item_query_count = item_query_id + ".count"
+        set_time_obj = {item_query_time: self._parse_rfc3339(time)}
+        set_count_obj = {item_query_count: {
+            "$sum": ["$" + item_query_count, 1]}}
+        collection_table = self._mongo_client[os.environ['METASPOREFLOW_TRACKING_DB_TABLE']]
+        collection_table.update_one(
+            {"$and": [{"_id": user_id}, {item_query_id: {"$exists": True}}]},
+            [
+                {"$set": set_time_obj},
+                {"$set": set_count_obj},
+            ],
+            upsert=True
+        )
+
+    def _parse_rfc3339(self, datetime_str: str) -> datetime:
+        try:
+            return datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%S.%f%z")
+        except ValueError:
+            return datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%S%z")
+
     def run_forever(self):
         # Configuring S3 Connection
         s3_bucket = (os.environ['S3_BUCKET_NAME'])
@@ -84,30 +110,35 @@ class LogsAPIHTTPExtension():
             # Process the received batches if any.
             while not self.queue.empty():
                 batch = self.queue.get_nowait()
-                s3_filename = (os.environ['AWS_LAMBDA_FUNCTION_NAME'])+'-'+(datetime.now().strftime('%Y-%m-%d-%H:%M:%S.%f'))+'.log'
+                s3_filename = 'tracking_log/' + (os.environ['AWS_LAMBDA_FUNCTION_NAME']) + '-' + (
+                    datetime.now().strftime('%Y-%m-%d-%H:%M:%S.%f')) + '.log'
                 try:
                     # Save to S3
                     s3.Bucket(s3_bucket).put_object(Key=s3_filename, Body=str(batch))
                     # Save to MongoDB
-                    if os.environ['METASPOREFLOW_TRACKING_DB_ENABLE'] == 'true' and os.environ['METASPOREFLOW_TRACKING_DB_TYPE'] == 'mongodb':
-                        for item in range(len(batch)):
-                            record_str = batch[item]['record']
-                            record = json.loads(record_str)
-                            self._save_collections_to_mongodb(record)
+                    for item in range(len(batch)):
+                        record_str = batch[item]['record']
+                        record = json.loads(record_str)
+                        user_id = record['user_id']
+                        item_id = record['item_id']
+                        time = batch[item]['time']
+                        self._update_tracking_user_bhv_to_mongodb(user_id, item_id, time)
                 except Exception as e:
                     raise Exception(f"Error sending log to S3 {e}") from e
+
+
 _REGISTRATION_BODY = {
     "events": ["INVOKE", "SHUTDOWN"],
 }
 
 # Subscribe to platform logs and receive them on ${local_ip}:4243 via HTTP protocol.
 
-TIMEOUT_MS = 1000 # Maximum time (in milliseconds) that a batch is buffered.
-MAX_BYTES = 262144 # Maximum size in bytes that the logs are buffered in memory.
-MAX_ITEMS = 10000 # Maximum number of events that are buffered in memory.
+TIMEOUT_MS = 1000  # Maximum time (in milliseconds) that a batch is buffered.
+MAX_BYTES = 262144  # Maximum size in bytes that the logs are buffered in memory.
+MAX_ITEMS = 10000  # Maximum number of events that are buffered in memory.
 
 _SUBSCRIPTION_BODY = {
-    "destination":{
+    "destination": {
         "protocol": "HTTP",
         "URI": f"http://sandbox:{RECEIVER_PORT}",
     },
@@ -119,11 +150,13 @@ _SUBSCRIPTION_BODY = {
     }
 }
 
+
 def main():
-#    print(f"extension.logs_api_http_extension: Starting Extension {_REGISTRATION_BODY} {_SUBSCRIPTION_BODY}")
+    #    print(f"extension.logs_api_http_extension: Starting Extension {_REGISTRATION_BODY} {_SUBSCRIPTION_BODY}")
     # Note: Agent name has to be file name to register as an external extension
     ext = LogsAPIHTTPExtension(os.path.basename(__file__), _REGISTRATION_BODY, _SUBSCRIPTION_BODY)
     ext.run_forever()
+
 
 if __name__ == "__main__":
     main()
--- python/metasporeflow/python/metasporeflow/tracking/aws/function/functionsrc/app.py
+++ python/metasporeflow/python/metasporeflow/tracking/aws/function/functionsrc/app.py
@@ -1,7 +1,22 @@
 import json
 
 def lambda_handler(event, context):
-	print(json.dumps(event['body']))
+	# print(json.dumps(event['body']))
+	body = event['body']
+	try:
+		body_dict = json.loads(body)
+	except Exception as e:
+		return {
+			'statusCode': 400,
+			'body': json.dumps('Error. Invalid Json String in Request: ' + str(e))
+		}
+	if not {'user_id', 'item_id'} < set(body_dict.keys()):
+		return {
+			'statusCode': 400,
+			'body': "Error. user_id, item_id are required in request json body"
+		}
+	# print log to extension
+	print(body)
 	return {
 		'statusCode': 200,
 		'body': json.dumps('Success Lambda Handler')
